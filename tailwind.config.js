1c85f6ca8a977785a989886b185d9b8bab7369ca
katherineqian <katheqian@gmail.com>
2022-01-31 04:31:16 -0600

    Finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [    "./pages/**/*.{js,ts,jsx,tsx}",    "./components/**/*.{js,ts,jsx,tsx}",  ],
   theme: {
-    extend: {},
+    screens: {
+      'xs': '470px',
+    },
+    extend: { 
+      colors: {
+        primary: "#474ccb"
+      },
+    },
   },
   plugins: [],
 }
